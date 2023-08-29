7bcb14da04f49812ac90f6556dcd68ebc1f69bcf
Samuel Mimram <smimram@gmail.com>
2023-08-29 11:38:42 +0200

    Option to update.

--- src/main.ml
+++ src/main.ml
@@ -1,13 +1,15 @@
 let () =
+  let update = ref false in
   let dir = ref "/tmp/metrics" in
   let fname = ref "metrics.html" in
   Arg.parse
     [
       "-i", Arg.Set_string dir, "Set directory containing metrics";
-      "-o", Arg.Set_string fname, "Set output file name"
+      "-o", Arg.Set_string fname, "Set output file name";
+      "--update", Arg.Set update, "Update data"
     ]
     (fun _ -> ()) "metrics [option]";
-  Metrics.load_liquidsoap ~directory:!dir ();
+  Metrics.load_liquidsoap ~update:!update ~directory:!dir ();
   let oc = open_out !fname in
   let w fmt = Printf.ksprintf (fun s -> output_string oc (s^"\n")) fmt in
   w "<html>";
