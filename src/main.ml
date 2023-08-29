ed30bfb0d97c604e6edae60b506f2696cb271c9a
Samuel Mimram <smimram@gmail.com>
2023-08-29 12:37:27 +0200

    Better style.

--- src/main.ml
+++ src/main.ml
@@ -14,14 +14,17 @@ let () =
   let w fmt = Printf.ksprintf (fun s -> output_string oc (s^"\n")) fmt in
   w "<html>";
   w "<head>";
+  w "<link rel=\"stylesheet\" href=\"metrics.css\">";
   w "</head>";
   w "<body>";
   w "<h1>Liquidsoap metrics</h1>";
   List.iter
     (fun (name, _category, _unit, data) ->
        Printf.printf "Plotting '%s'\n" name;
-       w "<h2>%s</h2>" name;
-       w "%s" (Plot.svg ~width:600. ~height:400. data)
+       w "<h2>%s</h2>" (String.capitalize_ascii name);
+       w "<div class=\"plot\">";
+       w "%s" (Plot.svg ~width:800. ~height:400. data);
+       w "</div>"
     )
     (Metrics.series ());
   w "</body>";
